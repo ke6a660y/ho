14a1a6f5003dffaa5fbd49e3892df5c261261056
ke6a660y <nerobovmsks@icloud.com>
2022-12-10 22:20:45 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ho/second.java
@@ -0,0 +1,14 @@
+package com.example.ho;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
